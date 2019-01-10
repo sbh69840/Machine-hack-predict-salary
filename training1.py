b24703948b5eab42d088471658233a614bb6cb70
dsu <sbh69840@gmail.com>
2019-01-10 12:07:01 +0530

    training1.py using sklearn randomforest

--- training1.py
+++ training1.py
@@ -38,14 +38,28 @@ test["location"] = test["location"].map(loc_to_index)
 
 train["salary"] = train["salary"].map(sal_to_index)
 
+exp_unique = train["experience"].unique()
+exp_to_index = {val:ind for ind,val in enumerate(exp_unique)}
+ind_to_exp = {ind:val for ind,val in enumerate(exp_unique)}
+train["experience"] = train["experience"].map(exp_to_index)
+test["experience"] = test["experience"].map(exp_to_index)
 
-X = train[["min_exp","max_exp","location"]].values 
+X = train[["min_exp","max_exp","location","company_name_encoded"\
+,"experience"]].values 
 y = train["salary"].values 
-y = to_categorical(y,6)
+# y = to_categorical(y,6)
 X_train,X_val,y_train,y_val = train_test_split(X,y,stratify=y,test_size=0.1)
-X_test = test[["min_exp","max_exp","location"]].values
+X_test = test[["min_exp","max_exp","location","company_name_encoded"\
+,"experience"]].values
 print(X_test.shape)
 
+#Build the model using randomforestclassifier
+from sklearn.ensemble import RandomForestClassifier 
+clf = RandomForestClassifier(n_estimators=1000, max_depth=4,random_state=0)
+clf.fit(X_train,y_train)
+print(clf.feature_importances_)
+print(clf.score(X_val,y_val))
+sadasd=asdas
 # X_train = np.expand_dims(X_train,axis=-1)
 # X_val = np.expand_dims(X_val,axis=-1)
 # X_test = np.expand_dims(X_test,axis=-1)
@@ -54,6 +68,7 @@ print(X_test.shape)
 # y_val = np.expand_dims(y_val,axis=-1)
 
 #Build the model using keras 
+
 from keras.models import Sequential 
 from keras.layers import Dense,Conv1D,MaxPooling1D,Flatten
 from keras.callbacks import ModelCheckpoint
